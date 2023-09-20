e82313a71526871118f8b4f11f10bcc9f2be9095
MaVCor13 <134764216+MaVCor13@users.noreply.github.com>
2023-09-20 10:01:58 -0300

    js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'; // Asegúrate de tener un archivo App.css en la misma carpeta
+
+const options = ["Piedra", "Papel", "Tijeras"];
+
+const getResult = (userChoice, computerChoice) => {
+  // Implementa tu lógica para determinar el resultado del juego aquí
+  // Devuelve "Empate", "Ganaste" o "Perdiste"
+};
 
 function App() {
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const handleChoice = (choice) => {
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
+    const gameResult = getResult(choice, computerChoice);
+
+    setPlayerChoice(choice);
+    setComputerChoice(computerChoice);
+    setResult(gameResult);
+  };
+
+  const resetGame = () => {
+    setPlayerChoice(null);
+    setComputerChoice(null);
+    setResult(null);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Juego de Piedra, Papel y Tijeras</h1>
+      <div>
+        {options.map((choice) => (
+          <button key={choice} onClick={() => handleChoice(choice)}>
+            {choice}
+          </button>
+        ))}
+      </div>
+      {playerChoice && computerChoice && (
+        <div>
+          <p>Tu elección: {playerChoice}</p>
+          <p>Elección de la computadora: {computerChoice}</p>
+          <p>Resultado: {result}</p>
+          <button onClick={resetGame}>Reiniciar</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
