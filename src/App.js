89a4322e80d7cc4fd91efcde02fd578e921f1b9e
MaVCor13 <134764216+MaVCor13@users.noreply.github.com>
2023-09-20 12:54:28 -0300

    gag

--- src/App.js
+++ src/App.js
@@ -1,151 +1,83 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
+import piedraImage from "./img/piedra.png";
+import papelImage from "./img/papel.png";
+import tijerasImage from "./img/tijeras.png";
 
 const options = [
-  { id: 0, name: "Piedra", emoji: "🪨", beats: [2] },
-  { id: 1, name: "Papel", emoji: "📄", beats: [0] },
-  { id: 2, name: "Tijera", emoji: "✂️", beats: [1] },
+  { id: 0, name: "Piedra", emoji: "🪨" },
+  { id: 1, name: "Papel", emoji: "📄" },
+  { id: 2, name: "Tijeras", emoji: "✂️" },
 ];
 
 const getResult = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
-    return 0; // Empate
+    return "Empate";
   }
 
-  if (options[userChoice].beats.includes(computerChoice)) {
-    return 1; // Ganaste
+  if (
+    (userChoice === "Piedra" && computerChoice === "Tijeras") ||
+    (userChoice === "Papel" && computerChoice === "Piedra") ||
+    (userChoice === "Tijeras" && computerChoice === "Papel")
+  ) {
+    return "Ganaste";
   }
 
-  return 2; // Perdiste
+  return "Perdiste";
 };
 
-function OptionButton({ option, handlePlay, disabled }) {
-  return (
-    <button
-      className="px-4 py-2 m-2 text-xl font-bold text-white bg-yellow-500 rounded-full hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-gray-400 disabled:opacity-50 disabled:cursor-not-allowed"
-      disabled={disabled}
-      onClick={() => handlePlay(option.id)}
-      title={option.name}
-    >
-      {option.emoji}
-    </button>
-  );
-}
-
-function useChoices() {
-  const [userChoice, setUserChoice] = useState(null);
+function App() {
+  const [playerChoice, setPlayerChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
-  const [userMessage, setUserMessage] = useState(null);
-  const [computerMessage, setComputerMessage] = useState(null);
   const [result, setResult] = useState(null);
-  const [disabled, setDisabled] = useState(false);
 
-  useEffect(() => {
-    if (userChoice !== null) {
-      setUserMessage(
-        `Has elegido ${options[userChoice]?.emoji} - ${options[userChoice]?.name}`
-      );
-    }
-  }, [userChoice]);
+  const handleChoice = (choice) => {
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
+    const gameResult = getResult(choice, computerChoice);
 
-  useEffect(() => {
-    if (computerChoice !== null) {
-      setComputerMessage(
-        `El ordenador ha elegido ${options[computerChoice]?.emoji} - ${options[computerChoice]?.name}`
-      );
-    }
-  }, [computerChoice]);
-
-  const handlePlay = (choice) => {
-    setUserChoice(choice);
-    setDisabled(true);
-    const randomChoice = Math.floor(Math.random() * 3);
-
-    setTimeout(() => {
-      setComputerChoice(randomChoice);
-    }, 1500);
-
-    setTimeout(() => {
-      setResult(getResult(choice, randomChoice));
-    }, 3000);
-
-    clearTimeout();
+    setPlayerChoice(choice);
+    setComputerChoice(computerChoice);
+    setResult(gameResult);
   };
 
-  const reset = () => {
-    setUserChoice(null);
+  const resetGame = () => {
+    setPlayerChoice(null);
     setComputerChoice(null);
-    setUserMessage(null);
-    setComputerMessage(null);
     setResult(null);
-    setDisabled(false);
   };
-
-  return {
-    userChoice,
-    computerChoice,
-    userMessage,
-    computerMessage,
-    result,
-    disabled,
-    handlePlay,
-    reset,
-  };
-}
-
-export default function Game() {
-  const {
-    userChoice,
-    computerChoice,
-    userMessage,
-    computerMessage,
-    result,
-    disabled,
-    handlePlay,
-    reset,
-  } = useChoices();
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-800">
-      <div className="rounded-lg p-4 bg-gray-500">
-        <h1 className="text-3xl mb-4 text-center font-bold">¡A jugar!</h1>
-        <div className="max-w-md mx-auto">
-          {options.map((option) => (
-            <OptionButton
-              key={option.id}
-              option={option}
-              handlePlay={handlePlay}
-              disabled={disabled}
-            />
-          ))}
-          {userChoice !== null && <p className="text-xl mt-4">{userMessage}</p>}
-          {computerChoice !== null && (
-            <p className="text-xl mt-4">{computerMessage}</p>
-          )}
-          {result !== null && (
-            <div className="mt-8">
-              {result === 0 && <p className="text-xl mt-4">🤷🏽‍♀️ Empate</p>}
-              {result === 1 && (
-                <p className="text-xl mt-4">
-                  ✅ Has ganado con {options[userChoice]?.name} contra{" "}
-                  {options[computerChoice]?.name}
-                </p>
-              )}
-              {result === 2 && (
-                <p className="text-xl mt-4">
-                  ❌ Has perdido con {options[userChoice]?.name} contra{" "}
-                  {options[computerChoice]?.name}
-                </p>
-              )}
-              <button
-                className="bg-yellow-500 hover:bg-yellow-700 text-black font-semibold py-2 px-4 mt-4 border-b-4 border-yellow-700"
-                onClick={reset}
-              >
-                Jugar de nuevo
-              </button>
-            </div>
-          )}
+    <div className="container">
+      <h1 className="header">Juego de Piedra, Papel y Tijeras</h1>
+      <div className="button-container">
+        {options.map((choice) => (
+          <button
+            key={choice.name}
+            onClick={() => handleChoice(choice.name)}
+            disabled={playerChoice !== null}
+            className="button"
+          >
+            {choice.name}
+          </button>
+        ))}
+      </div>
+      {playerChoice && computerChoice && (
+        <div className="result-container">
+          <p>Tu elección: {playerChoice}</p>
+          <p>Elección de la computadora: {computerChoice}</p>
+          <p>Resultado: {result}</p>
+          <button onClick={resetGame} className="button">
+            Reiniciar
+          </button>
         </div>
+      )}
+      <div className="images-container">
+        <img src={piedraImage} alt="Piedra" />
+        <img src={papelImage} alt="Papel" />
+        <img src={tijerasImage} alt="Tijeras" />
       </div>
     </div>
   );
 }
+
+export default App;
