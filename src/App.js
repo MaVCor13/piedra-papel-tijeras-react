fce3f620e1dc943d38e588280db346473fa80795
MaVCor13 <134764216+MaVCor13@users.noreply.github.com>
2023-09-20 21:05:00 -0300

    hhhha

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const options = [
@@ -24,6 +24,21 @@ const getResult = (userChoice, computerChoice) => {
 };
 
 function App() {
+  const [playerName, setPlayerName] = useState("");
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [round, setRound] = useState(0);
+  const maxRounds = 5;
+
+  const handleNameChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`¡Hola, ${playerName}!`);
+  };
+
   const [playerChoice, setPlayerChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
   const [result, setResult] = useState(null);
@@ -35,6 +50,14 @@ function App() {
     setPlayerChoice(choice);
     setComputerChoice(computerChoice);
     setResult(gameResult);
+
+    if (gameResult === "Ganaste") {
+      setPlayerScore(playerScore + 1);
+    } else if (gameResult === "Perdiste") {
+      setComputerScore(computerScore + 1);
+    }
+
+    setRound(round + 1);
   };
 
   const resetGame = () => {
@@ -43,15 +66,38 @@ function App() {
     setResult(null);
   };
 
+  useEffect(() => {
+    if (round === maxRounds) {
+      resetGame();
+      setPlayerScore(0);
+      setComputerScore(0);
+      setRound(0);
+    }
+  }, [round]);
+
   return (
     <div className="container">
       <h1 className="header">Juego de Piedra, Papel y Tijeras</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Ingresa tu nombre:
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleNameChange}
+          />
+        </label>
+        <button type="submit" className="button">
+          Comenzar
+        </button>
+      </form>
+
       <div className="button-container">
         {options.map((choice) => (
           <button
-            key={choice.name}
+            key={choice.id}
             onClick={() => handleChoice(choice.name)}
-            disabled={playerChoice !== null}
+            disabled={playerChoice !== null || round === maxRounds}
             className="button"
           >
             {choice.name}
@@ -61,24 +107,24 @@ function App() {
       {playerChoice && computerChoice && (
         <div className="result-container">
           <p>Tu elección: {playerChoice}</p>
-          <p>Elección de la computadora: {computerChoice}</p>
+          <p>Elección de la computadora: {computerChoice.name}</p>
           <p>Resultado: {result}</p>
-          <button onClick={resetGame} className="button">
-            Reiniciar
-          </button>
+          {round < maxRounds && (
+            <button onClick={resetGame} className="button">
+              Siguiente ronda
+            </button>
+          )}
         </div>
       )}
-      <div className="images-container">
-        <img src="/piedra.jpg" alt="Piedra" />
-        <img src="/papel.jpg" alt="Papel" />
-        <img src="/tijeras.jpg" alt="Tijeras" />
+      <div className="score-container">
+        <p>Marcador:</p>
+        <p>{playerName}: {playerScore}</p>
+        <p>Computadora: {computerScore}</p>
       </div>
     </div>
   );
 }
 
 export default App;
-
-
 
 
